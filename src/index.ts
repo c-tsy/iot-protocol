9880b5618cd8239bc87ff348a30d194a284aa9e5
CastleMY <490523604@qq.com>
2019-10-31 21:05:59 +0800

    修正Control的写入和读取以及explain解释过程的清空算法

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,7 @@ export enum ErrorType {
  * 基础对象
  */
 export class Base {
+    explain: string[] = [];
     /**
      * 版本号
      */
@@ -87,6 +88,7 @@ export class Base {
         let startIndex = this.buf.indexOf(0x68);
         if (startIndex > 0) { this.buf = this.buf.slice(startIndex); }
         this.Version = this.buf[1];
+        this.explain = [];
     }
 }
 /**
@@ -177,7 +179,6 @@ export class V0 extends Base {
 
     DataType: number = 0;
     Data: Buffer = Buffer.alloc(0);
-    explain: string[] = [];
     encode(explain: boolean = false): Buffer {
         let buf = [
             Buffer.from('68', 'hex'),
@@ -186,7 +187,7 @@ export class V0 extends Base {
             Buffer.alloc(4)
         ];
         //左移6位
-        buf[3].writeInt16LE(this.Control << 6, 0);
+        buf[3].writeUInt16BE(this.Control << 6, 0);
         //写入时间数据，携带数据或者加密模式都需要，协议包头包尾不加密
         if (this.Next) {
             buf[3][1] |= 1
@@ -195,7 +196,7 @@ export class V0 extends Base {
             buf[3][1] |= 2;
             let b = Buffer.alloc(4)
             let t = moment(this.Time).add(-10957, 'days').toDate().getTime() / 1000;
-            console.log(t)
+            // console.log(t)
             b.writeInt32LE(t, 0)
             buf.push(b)
         }
@@ -255,13 +256,13 @@ export class V0 extends Base {
     decode() {
         super.decode();
         //读出厂商代码
-        explain_text(this.buf, 0, 1, '起始位置', 'Start', '0x68')
-        explain_text(this.buf, 1, 1, '版本号', 'Version', this.Version);
+        this.explain.push(explain_text(this.buf, 0, 1, '起始位置', 'Start', '0x68'));
+        this.explain.push(explain_text(this.buf, 1, 1, '版本号', 'Version', this.Version));
         let i = 2;
         this._CompanyID = this.buf.slice(i, i += 3).toString('hex');
-        explain_text(this.buf, i - 3, 3, '厂商代码', 'CompanyID', this._CompanyID);
-        this.Control = this.buf.readUInt16LE(i) >> 6;
-        explain_text(this.buf, i, 2, '控制码(2位,编码左移,解码右移) >> 6', 'Control', this.Control);
+        this.explain.push(explain_text(this.buf, i - 3, 3, '厂商代码', 'CompanyID', this._CompanyID));
+        this.Control = this.buf.readUInt16BE(i) >> 6;
+        this.explain.push(explain_text(this.buf, i, 2, '控制码(2位,编码左移,解码右移) >> 6', 'Control', this.Control));
         let t = i + 4;
         i++;
         if (this.buf[i] & 1) {
@@ -269,58 +270,58 @@ export class V0 extends Base {
         }
         if (this.buf[i] & 2) {
             this.WithTime = true;
-            console.log(this.buf.readUInt32LE(t))
+            // console.log(this.buf.readUInt32LE(t));
             this.Time = moment(this.buf.readUInt32LE(t) * 1000).add(10957, 'days').toDate();
-            explain_text(this.buf, t, 4, '时间内容', 'Time', moment(this.Time).format('YYYY-MM-DD HH:mm:zz'));
+            this.explain.push(explain_text(this.buf, t, 4, '时间内容', 'Time', moment(this.Time).format('YYYY-MM-DD HH:mm:zz')));
             t += 4;
         }
-        explain_text(this.buf, i, 1, '是否带时间[第2位],1带0不带(带的情况下需要取后面的4字节时间)', 'WithTime', this.WithTime);
+        this.explain.push(explain_text(this.buf, i, 1, '是否带时间[第2位],1带0不带(带的情况下需要取后面的4字节时间)', 'WithTime', this.WithTime));
         if (this.buf[i] & 4) {
             this.Encrypted = true;
         }
-        explain_text(this.buf, i, 1, '是否加密[第3位],1加密0不加密', 'Encrypted', this.Encrypted);
+        this.explain.push(explain_text(this.buf, i, 1, '是否加密[第3位],1加密0不加密', 'Encrypted', this.Encrypted));
         if (this.buf[i] & 8) {
             this.Confirm = true;
         }
-        explain_text(this.buf, i, 1, '是否加密[第4位],1需要确认0不需确认', 'Confirm', this.Confirm);
-        explain_text(this.buf, i, 1, '接收方地址类型[第5位],1物理0逻辑', 'To.Type', '4字节为物理地址2字节为逻辑地址');
-        explain_text(this.buf, i, 1, '接收方地址类型[第6位],1物理0逻辑', 'From.Type', '4字节为物理地址2字节为逻辑地址');
+        this.explain.push(explain_text(this.buf, i, 1, '是否加密[第4位],1需要确认0不需确认', 'Confirm', this.Confirm));
+        this.explain.push(explain_text(this.buf, i, 1, '接收方地址类型[第5位],1物理0逻辑', 'To.Type', '4字节为物理地址2字节为逻辑地址'));
+        this.explain.push(explain_text(this.buf, i, 1, '接收方地址类型[第6位],1物理0逻辑', 'From.Type', '4字节为物理地址2字节为逻辑地址'));
         if (this.buf[i] & 32) {
             this.From.Type = AddressType.Phy;
-            this.From.Value = Buffer.from(this.buf.slice(t, t += 4)).reverse().toString('hex')
-            explain_text(this.buf, t - 4, 4, '发送方地址', 'From.Value', this.From.Value);
+            this.From.Value = Buffer.from(this.buf.slice(t, t += 4)).reverse().toString('hex');
+            this.explain.push(explain_text(this.buf, t - 4, 4, '发送方地址', 'From.Value', this.From.Value));
         } else {
             this.From.Type = AddressType.Logic;
-            this.From.Value = Buffer.from(this.buf.slice(t, t += 2)).reverse().toString('hex')
-            explain_text(this.buf, t - 2, 2, '发送方地址', 'From.Value', this.From.Value);
+            this.From.Value = Buffer.from(this.buf.slice(t, t += 2)).reverse().toString('hex');
+            this.explain.push(explain_text(this.buf, t - 2, 2, '发送方地址', 'From.Value', this.From.Value));
         }
         if (this.buf[i] & 16) {
             this.To.Type = AddressType.Phy;
-            this.To.Value = Buffer.from(this.buf.slice(t, t += 4)).reverse().toString('hex')
-            explain_text(this.buf, t - 4, 4, '接收方地址', 'To.Value', this.To.Value);
+            this.To.Value = Buffer.from(this.buf.slice(t, t += 4)).reverse().toString('hex');
+            this.explain.push(explain_text(this.buf, t - 4, 4, '接收方地址', 'To.Value', this.To.Value));
         } else {
             this.To.Type = AddressType.Logic;
-            this.To.Value = Buffer.from(this.buf.slice(t, t += 2)).reverse().toString('hex')
-            explain_text(this.buf, t - 2, 2, '接收方地址', 'To.Value', this.To.Value);
+            this.To.Value = Buffer.from(this.buf.slice(t, t += 2)).reverse().toString('hex');
+            this.explain.push(explain_text(this.buf, t - 2, 2, '接收方地址', 'To.Value', this.To.Value));
         }
         i++;
         //帧序号和帧内顺序
         this.ID = this.buf.readUInt16LE(i++) >> 7;
-        explain_text(this.buf, i - 1, 2, '帧序号 解码>>7,编码<<7', 'ID', this.ID);
+        this.explain.push(explain_text(this.buf, i - 1, 2, '帧序号 解码>>7,编码<<7', 'ID', this.ID));
         if (this.buf[i] & 64) {
             this.End = false;
         }
-        explain_text(this.buf, i, 1, '是否结束[第7位],1未结束0结束', 'End', this.End);
+        this.explain.push(explain_text(this.buf, i, 1, '是否结束[第7位],1未结束0结束', 'End', this.End));
         this.No = this.buf[i++] & 63;
-        explain_text(this.buf, i - 1, 1, '包内第几帧[低6位]', 'No', this.No);
+        this.explain.push(explain_text(this.buf, i - 1, 1, '包内第几帧[低6位]', 'No', this.No));
         i = t;
         /**
          * 数据区协议
          */
         this.DataType = this.buf[i++];
-        explain_text(this.buf, i - 1, 1, '数据区协议类型', 'DataType', this.DataType);
+        this.explain.push(explain_text(this.buf, i - 1, 1, '数据区协议类型', 'DataType', this.DataType));
         let len = this.buf.readUInt16LE(i);
-        explain_text(this.buf, i, 2, '数据区长度', 'DataLen', len);
+        this.explain.push(explain_text(this.buf, i, 2, '数据区长度', 'DataLen', len));
         i += 2;
         if (this.buf.length < i + len) {
             throw new Error(ErrorType.NOT_ENOUGH);
@@ -330,7 +331,7 @@ export class V0 extends Base {
             throw new Error(ErrorType.CRC_ERROR);
         }
         this.Data = this.buf.slice(i, i + len);
-        explain_text(this.buf, i, 2, '数据区内容', 'Data', buffer2hex(this.Data));
+        this.explain.push(explain_text(this.buf, i, 2, '数据区内容', 'Data', buffer2hex(this.Data)));
         return this;
     }
 }
